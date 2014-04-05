cf04ebe29d78e7077ead39cca067cf4aeeeec18e
pahans <pahan123@gmail.com>
2014-04-05 22:48:21 +0530

    ui integration

--- server/gcm.js
+++ server/gcm.js
@@ -8,19 +8,19 @@ BroadCastGCMMessage = function(message){
   var message = {
       registration_id: 'APA91bH4dxvUuiFzfhnxIHVkP0lQvcTWyreC1JDdcnIXMvvvySdsoGhURy7h6FEUOTP406904vrrS6K6v2EnEevClJcQfaYlGZqB7Vl88cn_Pm1waQ3U74uhbeazRbM346qTKH0uWhBlIZjhN2WBQN3F5Jy1S6EuddizKTCs-oyIa-Qrxmhnays', // required
       collapse_key: "1314", 
-      'data': {
-        trainId: "1314",
-        type: "delayed", // delayed /cancelled
-        trainDesc: "Matara - colombo",
-        delayTime:"15"
-      }
+      "data.trainId": "1314",
+      "data.type": "delayed", // delayed /cancelled
+      "data.trainDesc": "Matara - colombo",
+      "data.delayTime":"15"
   };
 
   gcm.send(message, function(err, messageId){
     if (err) {
+        return err;
         console.log("Something has gone wrong!");
     } else {
         console.log("Sent with message ID: ", messageId);
+        return messageId;
     }
   });
 }
