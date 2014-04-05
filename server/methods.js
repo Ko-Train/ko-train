598c44ce6fff52878d208d1342d1fe5b176e1a93
pahans <pahan123@gmail.com>
2014-04-05 15:37:42 +0530

    renamed shedule, added dummy data

---
+++ server/methods.js
@@ -0,0 +1,45 @@
+Meteor.methods({
+  "getShedule": function (date, start, end) {
+    // var soap = Meteor.require('soap-attrib');
+
+    // var url = 'http://lankagate.gov.lk:9080/services/RailwayWebServiceV2Proxy?wsdl';
+    // var args = {name: 'value'};
+    // soap.createClient(url, function(err, client) {
+    //   console.log('***************', err, client)
+    //     // client.MyFunction(args, function(err, result) {
+    //     //     console.log(result);
+    //     // });
+    // });
+
+    var result = [{
+      startStationArrivalTime: 1396691573895,
+      startStationDepartureTime: 1396691573895,
+      trainStartsFrom: "AVISAWELLA",
+      trainRunsTo: "COLOMBO FORT",
+      destReachingTime: 1396691573895,
+      availableDaysInWeek: "MONDAY TO FRIDAY", 
+      trainType: "COLOMBO COMMUTER",
+      trainNo: 1023,
+    }, {
+      startStationArrivalTime: 1396691573895,
+      startStationDepartureTime: 1396691573895,
+      trainStartsFrom: "AVISAWELLA",
+      trainRunsTo: "COLOMBO FORT",
+      destReachingTime: 1396691573895,
+      availableDaysInWeek: "MONDAY TO FRIDAY", 
+      trainType: "COLOMBO COMMUTER",
+      trainNo: 1023,
+    }, 
+    {
+      startStationArrivalTime: 1396691573895,
+      startStationDepartureTime: 1396691573895,
+      trainStartsFrom: "AVISAWELLA",
+      trainRunsTo: "COLOMBO FORT",
+      destReachingTime: 1396691573895,
+      availableDaysInWeek: "MONDAY TO FRIDAY", 
+      trainType: "COLOMBO COMMUTER",
+      trainNo: 1023,
+    }];
+    return result;
+  }
+});
