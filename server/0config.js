60e889eea675d4a8edaa25924005bf55c39c038a
pahans <pahan123@gmail.com>
2014-04-06 04:23:20 +0530

    rest api changes

--- server/0config.js
+++ server/0config.js
@@ -3,26 +3,16 @@ Config = {
 	'sms_gateway_password':'hackone'
 };
 
- Meteor.startup(function () {
 
-    // All values listed below are default
-    collectionApi = new CollectionAPI({
-      authToken: undefined,              // Require this string to be passed in on each request
-      apiPath: 'collectionapi',          // API path prefix
-    });
-
-    collectionApi.addCollection(Androidsubs, 'androidsubs', {
-      // All values listed below are default
-      authToken: undefined,                   // Require this string to be passed in on each request
-      methods: ['POST','GET','PUT'],  // Allow creating, reading, updating, and deleting
-      before: {  // This methods, if defined, will be called before the POST/GET/PUT/DELETE actions are performed on the collection. If the function returns false the action will be canceled, if you return true the action will take place.
-        POST: undefined,  // function(obj) {return true/false;},
-        GET: undefined,  // function(collectionID, objs) {return true/false;},
-        PUT: undefined,  //function(collectionID, obj, newValues) {return true/false;},
-        DELETE: undefined,  //function(collectionID, obj) {return true/false;}
+// API
+HTTP.methods({
+  '/collectionapi/androidsubs': {
+    method: function(data) {
+      var id = data.registrationId;
+      if(id){
+        console.log(id)
+        Androidsubs.insert({registrationId: id});
       }
-    });
-
-    // Starts the API server
-    collectionApi.start();
-  });
+    },
+  }
+});
