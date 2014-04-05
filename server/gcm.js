8923fc70a534327e9ef8c1a0a4c391c4cce7f960
pahans <pahan123@gmail.com>
2014-04-05 18:02:10 +0530

    GCM data structure

--- server/gcm.js
+++ server/gcm.js
@@ -1,6 +1,5 @@
 BroadCastGCMMessage = function(message){
   var GCM = Meteor.require('gcm').GCM;
-  console.log(GCM)
 
   var apiKey = 'AIzaSyA_iYqv4it9XSNm5f_fLY_rMbI1etPnETs';
   var gcm = new GCM(apiKey);
@@ -8,9 +7,13 @@ BroadCastGCMMessage = function(message){
   
   var message = {
       registration_id: 'APA91bGjymEXK3_QvEWRNGNeuOns6U9jP2nRkpr4frCeWFqKHjZapJmQbz_yVDA3vLhPkj72FOffYGK4NeaaXAqcxXo2UR8JNrrvAW1DTkgggKrR2Yj7H-iqYRb3AQVHdn3qabc91Ytq0z4woSH2yMih_ENKJ5R1EFi20XWmHDIss-RB4PLliQk', // required
-      collapse_key: 'Collapse key', 
-      'data.key1': 'value1',
-      'data.key2': 'value2'
+      collapse_key: "1314", 
+      'data': {
+        trainId: "1314",
+        type: "delayed", //delayed/cancelled
+        trainName: "Matara - colombo",
+        delayTime:"15 mins"
+      }
   };
 
   gcm.send(message, function(err, messageId){
